7351a080eb3c652758120a1ec78b80efee76ebe4
Kenta IDA <fuga@fugafuga.org>
2025-03-31 05:16:43 +0900

    Fix that pkg for 0.1.2 was missing.

---
+++ docs/axdl-gui/v0.1.2/pkg/README.md
@@ -0,0 +1,138 @@
+# axdl-rs Unofficial Axera image downloader implementation in Rust
+
+This is an unofficial Axera image downloader implementation in Rust to write image file into Axera SoCs.
+
+[日本語](./README.ja.md)
+
+## Table of Contents
+
+- [Prepare](#Prepare)
+- [Install](#Install)
+- [Web Browser Version](#web-browser-version)
+- [Build](#build)
+- [Usage](#usage)
+- [License](#license)
+
+## Prepare
+
+### Linux (Debian based)
+
+In order to access to the device from a normal user, you have to configure udev to allow a normal user to access the device.
+To configure udev, copy `99-axdl.rules` into `/etc/udev/rules.d` and reload the configuration of udev.
+
+```
+sudo cp 99-axdl.rules /etc/udev/rules.d/
+sudo udevadm control --reload
+```
+
+If the user is not in the plugdev group, add them to it and re-login. (Group membership changes require a re-login to take effect.)
+
+```
+id
+# Confirm that ...,(plugdev),... is included in the output
+```
+
+```
+# Add the user to the plugdev group
+sudo usermod -a -G plugdev $USER
+```
+
+Since this tool depends on libusb and libudev, install them in advance:
+
+```
+sudo apt install -y libudev-dev libusb-1.0-0-dev
+```
+
+## Install
+
+`axdl-cli` can be installed via `cargo install`.
+
+```
+cargo install axdl-cli
+```
+
+## Web Browser Version
+
+You can run the web browser version from [https://www.fugafuga.org/axdl-rs/axdl-gui/latest/](https://www.fugafuga.org/axdl-rs/axdl-gui/latest/).
+
+![axdl-gui](./doc/axdl-gui.drawio.svg)
+
+1. Click `Open Image` and select the `.axp` file you want to flash.
+2. If you don’t want to flash the rootfs, check `Exclude rootfs`.
+3. Click `Open Device` to open the USB device selection screen.
+4. Connect the Axera SoC to the host in download mode. (For M5Stack Module LLM, hold down the BOOT button while plugging in the USB cable.)
+5. While the Axera SoC is in download mode, click `Download`. (If it exits download mode within about 10 seconds, redo step (3).)
+
+## Build
+
+Before building the project, install the Rust toolchain via rustup.
+
+```bash
+# Clone the repository
+git clone https://github.com/ciniml/axdl-rs.git
+
+# Change directory
+cd axdl-rs
+```
+### Building the Command-Line Version
+
+```
+# Build
+cargo build --bin axdl-cli --package axdl-cli
+```
+
+### Building the Web Browser Version
+
+To build the web browser version, install wasm-pack:
+
+```
+cargo install wasm-pack
+```
+
+Then build with wasm-pack:
+
+```
+cd axdl-gui
+wasm-pack build --target web --release
+```
+
+## Usage
+
+To burn a *.axp image, run the command below and plug the Axera SoC device with download mode.
+For M5Stack Module LLM, keep press the BOOT button and plug the USB cable into the device.
+
+```shell
+cargo run --bin axdl-cli --package axdl-cli -- --file /path/to/image.axp --wait-for-device
+```
+
+If you don't want to burn the rootfs, specify `--exclude-rootfs` option.
+
+```shell
+cargo run --bin axdl-cli --package axdl-cli -- --file /path/to/image.axp --wait-for-device --exclude-rootfs
+```
+
+On Windows or other platforms where the official Axera AXDL driver is installed, you can use serial port access by specifying the --transport serial option:
+
+```shell
+cargo run --bin axdl-cli --package axdl-cli -- --file /path/to/image.axp --wait-for-device --transport serial
+```
+
+### Web Browser Version
+
+After building, start a local HTTP server and access it from your browser. 
+A browser supporting WebUSB (such as Chrome) is required. 
+Below is an example of using Python’s HTTP module:
+
+```
+# Build the web browser version
+cd axdl-gui
+wasm-pack build --target web --release
+# Start the HTTP server
+python -m http.server 8000
+```
+
+Access http://localhost:8000 to open the web browser version.
+
+## License
+
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.
---
+++ docs/axdl-gui/v0.1.2/pkg/axdl_gui.d.ts
@@ -0,0 +1,103 @@
+/* tslint:disable */
+/* eslint-disable */
+export function main(): void;
+/**
+ * The `ReadableStreamType` enum.
+ *
+ * *This API requires the following crate features to be activated: `ReadableStreamType`*
+ */
+type ReadableStreamType = "bytes";
+export class IntoUnderlyingByteSource {
+  private constructor();
+  free(): void;
+  start(controller: ReadableByteStreamController): void;
+  pull(controller: ReadableByteStreamController): Promise<any>;
+  cancel(): void;
+  readonly type: ReadableStreamType;
+  readonly autoAllocateChunkSize: number;
+}
+export class IntoUnderlyingSink {
+  private constructor();
+  free(): void;
+  write(chunk: any): Promise<any>;
+  close(): Promise<any>;
+  abort(reason: any): Promise<any>;
+}
+export class IntoUnderlyingSource {
+  private constructor();
+  free(): void;
+  pull(controller: ReadableStreamDefaultController): Promise<any>;
+  cancel(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly main: () => void;
+  readonly slint_qt_get_widget: (a: number) => number;
+  readonly slint_mock_elapsed_time: (a: bigint) => void;
+  readonly slint_get_mocked_time: () => bigint;
+  readonly slint_send_mouse_click: (a: number, b: number, c: number) => void;
+  readonly slint_send_keyboard_char: (a: number, b: number, c: number) => void;
+  readonly send_keyboard_string_sequence: (a: number, b: number) => void;
+  readonly rust_zstd_wasm_shim_qsort: (a: number, b: number, c: number, d: number) => void;
+  readonly rust_zstd_wasm_shim_malloc: (a: number) => number;
+  readonly rust_zstd_wasm_shim_memcmp: (a: number, b: number, c: number) => number;
+  readonly rust_zstd_wasm_shim_calloc: (a: number, b: number) => number;
+  readonly rust_zstd_wasm_shim_free: (a: number) => void;
+  readonly rust_zstd_wasm_shim_memcpy: (a: number, b: number, c: number) => number;
+  readonly rust_zstd_wasm_shim_memmove: (a: number, b: number, c: number) => number;
+  readonly rust_zstd_wasm_shim_memset: (a: number, b: number, c: number) => number;
+  readonly __wbg_intounderlyingbytesource_free: (a: number, b: number) => void;
+  readonly intounderlyingbytesource_type: (a: number) => number;
+  readonly intounderlyingbytesource_autoAllocateChunkSize: (a: number) => number;
+  readonly intounderlyingbytesource_start: (a: number, b: any) => void;
+  readonly intounderlyingbytesource_pull: (a: number, b: any) => any;
+  readonly intounderlyingbytesource_cancel: (a: number) => void;
+  readonly __wbg_intounderlyingsource_free: (a: number, b: number) => void;
+  readonly intounderlyingsource_pull: (a: number, b: any) => any;
+  readonly intounderlyingsource_cancel: (a: number) => void;
+  readonly __wbg_intounderlyingsink_free: (a: number, b: number) => void;
+  readonly intounderlyingsink_write: (a: number, b: any) => any;
+  readonly intounderlyingsink_close: (a: number) => any;
+  readonly intounderlyingsink_abort: (a: number, b: any) => any;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_1: WebAssembly.Table;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_6: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9e78a6698996d1e1: (a: number, b: number) => void;
+  readonly closure1026_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure1432_externref_shim: (a: number, b: number, c: any) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5114c3094c245bc7: (a: number, b: number) => void;
+  readonly closure1437_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha2dacd4443abe25a: (a: number, b: number) => void;
+  readonly closure2728_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure2767_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure2789_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
